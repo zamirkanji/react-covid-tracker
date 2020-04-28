7d9c78396ce7044b3445def4d8a193d1f05c5820
Zamir Kanji <zamirkanji@comcast.net>
2020-04-27 20:42:59 -0700

    Updates

---
+++ precache-manifest.22ce09e46d6e971882eeddc0fc196b95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5f4da2a3ebdcbd4c92c48d8410276fd",
+    "url": "/react-covid-tracker/index.html"
+  },
+  {
+    "revision": "75f5563ac3e7fedfdc4e",
+    "url": "/react-covid-tracker/static/css/main.f6d3e361.chunk.css"
+  },
+  {
+    "revision": "4b754448daac465b8a13",
+    "url": "/react-covid-tracker/static/js/2.a1256583.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react-covid-tracker/static/js/2.a1256583.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75f5563ac3e7fedfdc4e",
+    "url": "/react-covid-tracker/static/js/main.a59916fe.chunk.js"
+  },
+  {
+    "revision": "6bfb872f1e3bb72e3ccc",
+    "url": "/react-covid-tracker/static/js/runtime-main.a903252e.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/react-covid-tracker/static/media/image.d7265326.png"
+  }
+]);
